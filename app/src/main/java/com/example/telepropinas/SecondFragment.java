49dbd21da95c60952010789e37b40eb14688b636
Nestorcanpac <nescanpac@alu.edu.gva.es>
2024-01-19 13:49:49 +0100

    Interfaz Fea

--- app/src/main/java/com/example/telepropinas/SecondFragment.java
+++ app/src/main/java/com/example/telepropinas/SecondFragment.java
@@ -1,9 +1,11 @@
 package com.example.telepropinas;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -24,19 +26,25 @@ public class SecondFragment extends Fragment {
         binding = FragmentSecondBinding.inflate(inflater, container, false);
         return binding.getRoot();
 
+
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(SecondFragment.this)
-                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
-            }
-        });
+
+        Button boton=view.findViewById(R.id.FormularioBoton);
+        boton.setBackgroundColor(Color.parseColor("#FFFFFF"));
+
+        Button boton2=view.findViewById(R.id.MapaBoton);
+        boton2.setBackgroundColor(Color.parseColor("#FFFFFF"));
+
+
+        binding.FormularioBoton.setOnClickListener(v -> {
+          binding.IntroducePropiTXT.setText(binding.IntroduceDireccion.getText());
+      });
     }
+
 
     @Override
     public void onDestroyView() {
